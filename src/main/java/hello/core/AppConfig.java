c54b0798515de235e9c333099655476d1cc167a5
jdohun <wjdehgns97@naver.com>
2022-01-02 16:00:35 +0900

    의존관계 자동 주입 - 44강 4가지 방법 생성자, 수정자(setter), 필드, 일반 메서드

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -39,9 +39,7 @@ public class AppConfig {
     @Bean
     public OrderService orderService(){
         System.out.println("call AppConfig.orderService");
-        return new OrderServiceImpl(
-                memberRepository(),
-                discountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
